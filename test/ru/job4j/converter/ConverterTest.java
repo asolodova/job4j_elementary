f0a7c0c25c86428bcdae17ad4d72e5b22f42e20f
solodova <a.solodova@adsterratech.com>
2020-05-01 13:08:14 +0300

    1. Модульные тесты. [#282653]

---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.converter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    // Euro
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void whenConvertMinus180RblThenMinus3Eur() {
+        int in = -180;
+        int expected = -2;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void whenConvert0RblThen0Eur() {
+        int in = 0;
+        int expected = 0;
+        int out = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, out);
+    }
+
+    //Dollar
+    @Test
+    public void whenConvert180RblThen3Dlr() {
+        int in = 180;
+        int expected = 3;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void whenConvertMinus180RblThenMinus3Dlr() {
+        int in = -180;
+        int expected = -3;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void whenConvert0RblThen0Dlr() {
+        int in = 0;
+        int expected = 0;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void whenConvert10RblThen0Dlr() {
+        int in = 10;
+        int expected = 0;
+        int out = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, out);
+    }
+}
